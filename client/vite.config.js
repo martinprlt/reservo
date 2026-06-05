d99e255dfe48e280959f25c9810bd2e0a505aba1
martinprlt <martinprlt02@gmail.com>
2026-06-04 22:48:43 -0300

    fix: remove Google Fonts SW caching - fonts loaded via link tags SW fetch conflicts with CSP

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,11 +11,6 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: { cacheName: 'google-fonts-cache', expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 } }
-          },
-          {
             urlPattern: /\/api\/.*/i,
             handler: 'NetworkFirst',
             options: { cacheName: 'api-cache', networkTimeoutSeconds: 5 }
